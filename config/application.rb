015a6ad49116f7bef1241b70f2bae2f92a4f7f35
Tom Arra <tarra3@gmail.com>
2012-10-19 08:17:50 -0500

    Updates for v1.3

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,6 @@ module WeddingWebsite
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.2'
+    config.assets.version = '1.3'
   end
 end
