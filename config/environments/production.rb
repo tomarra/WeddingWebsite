0fd7959e81a3c80e36676dec90776fb9df45b49a
Tom Arra <tarra3@gmail.com>
2013-01-06 18:27:42 -0600

    Trying to fix ActiveAdmin on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ WeddingWebsite::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # CHANGED TO TRUE FOR ACTIVE ADMIN
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
