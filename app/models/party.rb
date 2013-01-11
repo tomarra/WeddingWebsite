81b046e631ae3ea6998adf01c4064e61d232a21a
Tom Arra <tarra3@gmail.com>
2013-01-10 20:04:25 -0600

    Adding some validation to the models

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,13 @@
 class Party < ActiveRecord::Base
   attr_accessible :address, :city, :name, :state, :zip_code
+
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip_code
+
+  validates_length_of :zip_code, :minimum => 5, :too_short => "Zip code is at least 5 digits"
+
+  validates_numericality_of :zip_code, :only_integer
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,6 @@ class Response < ActiveRecord::Base
   belongs_to :person
 
   attr_accessible :attending, :meal, :meal_notes, :person_id
+
+  validates_presence_of :attending
 end
