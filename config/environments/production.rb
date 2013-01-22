4d858483de6923096416f830c936fe8480453434
Tom Arra <tarra3@gmail.com>
2013-01-21 18:38:23 -0600

    Trying to fix heroku-test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ WeddingWebsite::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # CHANGED TO TRUE FOR ACTIVE ADMIN
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
