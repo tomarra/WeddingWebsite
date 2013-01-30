4bfe86e9b8203cb73ee986a31c473569d72f10d9
Tom Arra <tarra3@gmail.com>
2013-01-29 18:20:18 -0600

    More RSVP form and data updates.

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,4 +8,22 @@ class Person < ActiveRecord::Base
   attr_accessible :first_name, :guest_allowed, :guest_person_id, :is_guest, :last_name, :party_id, :attending, :meal, :meal_notes, :bringing_guest
 
   validates_presence_of :party_id
+  validate :validate_person
+
+  def validate_person
+  	#First set if they are attending or not
+  	if self.is_guest && self.first_name != "" && self.last_name != "" && self.meal != ""
+  		self.attending = true
+  	end
+
+  	#If they are not attending then they dont get a meal in the DB
+  	if !self.attending
+  		self.meal = ""
+  	end
+
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
