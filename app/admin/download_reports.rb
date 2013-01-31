c4f85e18551a024aea43969a9ac3576be7529a74
Tom Arra <tarra3@gmail.com>
2013-01-30 20:38:31 -0600

    Making things a bit more pretty

--- app/admin/download_reports.rb
+++ app/admin/download_reports.rb
@@ -4,17 +4,16 @@ ActiveAdmin.register_page "Download Reports" do
 	end
 
 	action_item do
-      link_to "Download", "/admin/download_reports/download.xls"
+      link_to "Download", "/admin/download_reports/full_report.xls"
     end
 
     controller do
-    	def download
-    		@parties = Party.all
+    	def full_report
+    		@parties = Party.find(:all, :order => "name ASC")
 
 		    respond_to do |format|
-		      format.xls
+		      format.xls { render :file => "admin/download_reports/report.xls.erb" }
 		    end
-
     	end
     end
 end
