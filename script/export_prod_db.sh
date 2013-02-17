1c64de42008df29f6c8505c6ddc8570410d9a605
Tom Arra <tarra3@gmail.com>
2013-02-17 15:55:07 -0600

    Adding an easy way to get an import the latest production DB

---
+++ script/export_prod_db.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#$1 = Heroku App Name
+#$2 = DB Username
+
+heroku pgbackups:capture --app $1
+curl $(heroku pgbackups:url --app $1) > latest.dump
+dropdb 'prod_dump'
+createdb 'prod_dump'
+pg_restore --verbose --clean --no-acl --no-owner --h localhost -U $2 -d prod_dump latest.dump
+rm -f lastest.dump
